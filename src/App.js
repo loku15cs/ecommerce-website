fcf0775516078e829ae9c00f654eaeed84434215
Shubhansu <shubhansu.kumar@innodeed.com>
2023-09-05 02:18:27 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
+import Products from "./Products"
+import SingleProduct from "./SingleProduct"
+import Cart from "./Cart"
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="Contact" element={<Contact/>}/>
+        <Route path="Products" element={<Products/>}/>
+        <Route path="SingleProduct" element={<SingleProduct/>}/>
+        <Route path="Cart" element={<Cart/>}/>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
